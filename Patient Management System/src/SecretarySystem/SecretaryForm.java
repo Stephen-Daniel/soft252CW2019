d7a06387faf12b39bc41bb38142740a8640a2cb9
Stephen Daniel <stephen.daniel@students.plymouth.ac.uk>
2019-12-16 17:12:51 +0000

    working on using netbeans

--- Patient Management System/src/SecretarySystem/SecretaryForm.java
+++ Patient Management System/src/SecretarySystem/SecretaryForm.java
@@ -114,9 +114,6 @@ public class SecretaryForm {
 		frame.dispose();
 		
 	}
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private void initialize() {
